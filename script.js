72a2773ad37379e363870c97c08d4bf839d2c27c
kevsmartini <79332227+kevsmartini@users.noreply.github.com.>
2024-12-15 20:47:27 +0000

    custom tip input error fixed

--- script.js
+++ script.js
@@ -5,72 +5,61 @@ const customTip = document.querySelector(".custom-tip");
 const inputWarning = document.querySelector(".input-warning");
 const tipNumber = document.querySelector(".tip-number");
 const totalPersonNumber = document.querySelector(".total-number");
-const resetBtn = document.querySelector(".reset-button")
+const resetBtn = document.querySelector(".reset-button");
 
-
-const tipcalculator = (bill,tip,people)=>{
-  percentageOfBill = bill * (tip/100)
-  tipNumber.textContent = Math.round((percentageOfBill/people)*100)/100
-  totalPersonNumber.textContent = Math.round((bill+ percentageOfBill)/people*100)/100
-
-
-}
+const tipcalculator = (bill, tip, people) => {
+  percentageOfBill = bill * (tip / 100);
+  tipNumber.textContent = Math.round((percentageOfBill / people) * 100) / 100;
+  totalPersonNumber.textContent =
+    Math.round(((bill + percentageOfBill) / people) * 100) / 100;
+};
 
 percentageBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
-    const peopleNumber =  Number(peopleNumberInput.value)
-    const billValue = Number(bill.value)
-    const btnpercentageAmount = Number(btn.id)
+    const peopleNumber = Number(peopleNumberInput.value);
+    const billValue = Number(bill.value);
+    const btnpercentageAmount = Number(btn.id);
 
-    if (peopleNumber === 0 || peopleNumber === "" ) {
+    if (peopleNumber === 0 || peopleNumber === "") {
       inputWarning.style.display = "block";
-
-
-    
-    }else{
-      percentageBtn.forEach((btn) => btn.style.backgroundColor = 'var(--Very-dark-cyan)')
-      btn.style.backgroundColor = 'var(--Strong-cyan)'
-      tipcalculator(billValue,btnpercentageAmount,peopleNumber)
-
+    } else {
+      percentageBtn.forEach(
+        (btn) => (btn.style.backgroundColor = "var(--Very-dark-cyan)")
+      );
+      btn.style.backgroundColor = "var(--Strong-cyan)";
+      tipcalculator(billValue, btnpercentageAmount, peopleNumber);
     }
-
-   
-    
   });
 });
 
-customTip.addEventListener('keydown',(e)=>{
-  if (e.key === "Enter"){
-    customTip.id = customTip.value
-  customPercentageAmount = customTip.id
-  const peopleNumber =  Number(peopleNumberInput.value)
-  const billValue = Number(bill.value)
-  tipcalculator(billValue,customPercentageAmount,peopleNumber)
-
-
+customTip.addEventListener("keydown", (e) => {
+  if (
+    e.key === "Enter" &&
+    customTip.value != "" &&
+    bill.value != "" &&
+    peopleNumberInput.value != ""
+  ) {
+    customTip.id = customTip.value;
+    customPercentageAmount = customTip.id;
+    const peopleNumber = Number(peopleNumberInput.value);
+    const billValue = Number(bill.value);
+    tipcalculator(billValue, customPercentageAmount, peopleNumber);
   }
+});
 
-})
+peopleNumberInput.addEventListener("focus", (e) => {
+  e.target.value = "";
+  inputWarning.style.display = "none";
+});
 
-
-peopleNumberInput.addEventListener('focus',(e)=>{
-    e.target.value = ''
-    inputWarning.style.display = "none";
-
-})
-
-resetBtn.addEventListener('click',()=>{
+resetBtn.addEventListener("click", () => {
   percentageBtn.forEach((btn) => {
-    btn.style.backgroundColor = 'var(--Very-dark-cyan)'
+    btn.style.backgroundColor = "var(--Very-dark-cyan)";
+  });
 
-  })
-
-  peopleNumberInput.value= ''
-  bill.value = ''
-  tipNumber.textContent= '$0.00'
-  totalPersonNumber.textContent= '$0.00'
-  customTip.value = ''
-
-
-
-})
+  peopleNumberInput.value = "";
+  bill.value = "";
+  tipNumber.textContent = "$0.00";
+  totalPersonNumber.textContent = "$0.00";
+  customTip.value = "";
+});
